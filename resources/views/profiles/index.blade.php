1986749c8188ac8a33f3eed224a2ebd9163c3094
nishida <k.nishida@vistage-llc.com>
2020-08-15 15:30:46 +0900

    Restricting/Authorizing Actions with a Model Policy

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -9,8 +9,16 @@
         <div class="col-9">
             <div class="d-flex justify-content-between align-items-baseline">
                 <h1>{{ $user->username}}</h1>
+                @can('update',$user->profile)
                 <a href="/p/create">Add New Post</a>
+                @endcan
             </div>
+
+            @can('update',$user->profile)
+            <a href="/profile/{{$user->id}}/edit">Edit Profile</a>
+            @endcan
+
+
             <div class="d-flex">
                 <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
                 <div class="pr-5"><strong>23k</strong> follwers</div>
@@ -19,6 +27,8 @@
             <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
             <div>{{ $user->profile->description}}</div>
             <div><a href="#">{{$user->profile->url}}</a></div>
+            {{-- www.freecodecamp.org --}}
+
         </div>
     </div>
 
